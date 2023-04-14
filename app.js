a92b22926cccb53c525e6cada55c1fc7e0cbe8aa
Retief Ryke <retief@Retiefs-MacBook-Pro.local>
2023-04-14 13:20:00 +0200

    updated ports

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+
 mongoose.connect('mongodb+srv://oranjuicy:3PI536pnlLyp5e2u@todolist.vipuapr.mongodb.net/todoListDB');
 
 let items = [];
@@ -116,8 +117,11 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-let port = port.env.PORT
-if (port == null || port == "") {
+let port = process.env.PORT;
+if (port == null || port == ""){
   port = 3000;
 }
- app.listen(port)
+
+app.listen(port)
+
+
